d1f2806f081618ae616bfc0d484421fcf74861c5
labiras <labirasifpi@gmail.com>
2024-06-06 12:20:31 -0300

    global criado

--- JogoMNR/scripts/PauseScreen.gd
+++ JogoMNR/scripts/PauseScreen.gd
@@ -4,7 +4,7 @@ func _ready():
 	visible = false
 
 func _process(_delta):
-	if Input.is_action_just_pressed("ui_cancel"):
+	if Input.is_action_just_pressed("ui_cancel") and Global.can_pause:
 		pause_or_resume()
 		$VBoxContainer/Resumir.grab_focus()
 		
