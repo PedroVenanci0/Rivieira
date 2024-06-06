118b8c380573ade605b58d42b01e0f3e5baaf102
labiras <labirasifpi@gmail.com>
2024-06-06 13:07:30 -0300

    atualizando

--- JogoMNR/scripts/PauseScreen.gd
+++ JogoMNR/scripts/PauseScreen.gd
@@ -1,4 +1,18 @@
 extends Control
 
 func _ready():
-	$Pause/VBoxContainer/Resumir.grab_focus()
+	visible = false
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_cancel") and Global.can_pause:
+		pause_or_resume()
+		$VBoxContainer/Resumir.grab_focus()
+		
+
+
+func _on_Resumir_pressed():
+	pause_or_resume()
+	
+func pause_or_resume():
+	visible = !visible
+	get_tree().paused = !get_tree().paused 
