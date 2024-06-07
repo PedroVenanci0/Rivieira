4bad5b88d9d097c252d4fed05b398d2cff374bf7
RavCarvalho <ravennacarvalho098@gmail.com>
2024-06-07 13:47:57 -0300

    bordas de mundo criadas

--- JogoMNR/scripts/utils/AreaDeColeta.gd
+++ JogoMNR/scripts/utils/AreaDeColeta.gd
@@ -1,6 +1,8 @@
 extends Area2D
 onready var player = $".."
 
+signal lixo_out
+
 func _process(_delta):
 	var lixos_proximos = get_overlapping_bodies() # pega quantos lixos tem ao redor dele
 	if lixos_proximos.size() > 0: # se tiver lixo ao redor
@@ -10,4 +12,11 @@ func _process(_delta):
 			< lixo_mais_proximo.global_position.distance_to(self.global_position): # atualiza o lixo mais proximo
 				lixo_mais_proximo = lixo
 		
-		lixo_mais_proximo.queue_free() # da queue free no lixo mais proximo, alterar isso dps
+		if lixo_mais_proximo.has_method("aumentar_progresso") and Input.is_action_pressed("collect"): lixo_mais_proximo.aumentar_progresso()
+		
+#		lixo_mais_proximo.queue_free() # da queue free no lixo mais proximo, alterar isso dps
+
+
+func _on_AreaDeColeta_body_exited(body):
+	if body != null: body.resetar_progresso();
+	print("alguem saiu de mim")
---
+++ JogoMNR/scripts/utils/LimiteLixo.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+
+func _ready():
+	pass
+	
+
+
+func _on_LimiteLixo_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if body.has_method("resetar_progresso"):
+		print("Um lixo entrou em mim");
+		body.queue_free()
+
